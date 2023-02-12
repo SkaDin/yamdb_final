2785f80c27d4be173aa250b82f062ebf3794419d
Denis Syshkov <ga.akam@yandex.ru>
2023-02-12 13:22:32 +0300

    gggggg

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv('SECRET', default='t&e%000000kseeeyn^##co)opr@4zqj=rq&agd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'web', '84.201.152.177']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
